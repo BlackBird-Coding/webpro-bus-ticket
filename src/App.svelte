9385f6e76153cc218b72be7846b880230ccbd89b
Bla_ckB <50193121+BlackBird-Coding@users.noreply.github.com>
2024-10-02 11:39:01 +0700

    Add svelte-routing package and update dependencies

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,14 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+  import { Router, Route, links } from 'svelte-routing';
+  import Home from './routes/Home.svelte';
+  import Test from './routes/Test.svelte';
 </script>
 
-<main class="flex flex-col items-center text-center space-y-6">
-  <div class="flex space-x-6">
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="w-24 h-24" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="w-24 h-24" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1 class="text-4xl font-bold">Vite + Svelte</h1>
-
-  <div class="card p-6 bg-gray-100 rounded-lg shadow-lg">
-    <Counter />
-  </div>
-
-  <p class="text-lg">
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer" class="text-blue-500 underline">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs text-gray-600 italic">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+<main use:links>
+  <Router>
+    <button class="bg-slate-400 text-white p-5  border-4 rounded-md"><a href="/test">test</a></button>
+    <button class="bg-green-600 text-white p-5  border-4 rounded-md"><a href="/">home</a></button>
+    <Route path="/test" component={Test} />
+    <Route path="/"><Home /></Route>
+  </Router>
 </main>
