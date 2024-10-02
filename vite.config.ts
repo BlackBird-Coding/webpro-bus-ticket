3aeaee9cc2289b015c93f7e104a8c06713e65e8d
Bla_ckB <50193121+BlackBird-Coding@users.noreply.github.com>
2024-10-02 15:14:11 +0700

    Refactor vite.config.ts to add path resolution for aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import path from 'path'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte({
       compilerOptions: {
-        hydratable: true
-      }
-    })
-  ],resolve: {
+        hydratable: true,
+      },
+    }),
+  ],
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-})
+});
