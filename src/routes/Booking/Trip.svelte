240ee72052e518223361b5ff4a312a6a37716c2a
Jame <araya.saeyang2520@gmail.com>
2024-10-03 16:08:56 +0700

    edit trip

--- src/routes/Booking/Trip.svelte
+++ src/routes/Booking/Trip.svelte
@@ -1,108 +1,76 @@
+<script>
+  import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Radio} from 'flowbite-svelte';
+</script>
+
 <div class="m-10 mx-20">
   <div class="text-3xl">เลือกเที่ยวรถ</div>
   <div class="mt-5">
     <div class="text-2xl mb-5">เที่ยวไป</div>
-    <div class="flex flex-col gap-3">
-      <div class="bg-orange-500 flex justify-between py-1 text-white text-xl">
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >รถออก</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >เส้นทาง</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >ที่นั่งว่าง</label
-        >
-        <label class="w-1/3 flex justify-center items-center mr-9" for=""
-          >ราคา</label
-        >
-      </div>
-      <div class="border flex justify-between py-5 text-xl">
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >06:30</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >กรุงเทพฯ(หมอชิต2) - หาดใหญ่</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for="">100</label>
-        <label class="w-1/3 flex justify-center items-center" for="">300</label>
-        <input type="radio" class="mr-5 aspect-square w-5" name="go" id="" />
-      </div>
-      <div class="border flex justify-between py-5 text-xl">
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >06:30</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >กรุงเทพฯ(หมอชิต2) - หาดใหญ่</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for="">100</label>
-        <label class="w-1/3 flex justify-center items-center" for="">300</label>
-        <input type="radio" class="mr-5 aspect-square w-5" name="go" id="" />
-      </div>
-      <div class="border flex justify-between py-5 text-xl">
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >06:30</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >กรุงเทพฯ(หมอชิต2) - หาดใหญ่</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for="">100</label>
-        <label class="w-1/3 flex justify-center items-center" for="">300</label>
-        <input type="radio" class="mr-5 aspect-square w-5" name="go" id="" />
-      </div>
-    </div>
+    <Table hoverable={true}>
+        <TableHead class="bg-[#4F75FF] rounded-md py-2 text-lg text-center text-white">
+            <TableHeadCell>รถออก</TableHeadCell>
+            <TableHeadCell>เส้นทาง</TableHeadCell>
+            <TableHeadCell>ที่นั่งว่าง</TableHeadCell>
+            <TableHeadCell>ราคา</TableHeadCell>
+            <TableHeadCell>
+                <span class="sr-only">Action</span>
+            </TableHeadCell>
+        </TableHead>
+        <TableBody>
+            <TableBodyRow class="text-center">
+              <TableBodyCell>06:30</TableBodyCell>
+              <TableBodyCell>กรุงเทพฯ(หมอชิต2) - หาดใหญ่</TableBodyCell>
+              <TableBodyCell>100</TableBodyCell>
+              <TableBodyCell>300.00 บาท</TableBodyCell>
+              <TableBodyCell>
+                <Radio name="go"></Radio>
+              </TableBodyCell>
+            </TableBodyRow>
+            <TableBodyRow class="text-center">
+              <TableBodyCell>06:30</TableBodyCell>
+              <TableBodyCell>กรุงเทพฯ(หมอชิต2) - หาดใหญ่</TableBodyCell>
+              <TableBodyCell>100</TableBodyCell>
+              <TableBodyCell>300.00 บาท</TableBodyCell>
+              <TableBodyCell>
+                <Radio name="go"></Radio>
+              </TableBodyCell>
+            </TableBodyRow>
+        </TableBody>
+    </Table>
   </div>
   <div class="mt-5">
     <div class="text-2xl mb-5">เที่ยวกลับ</div>
-    <div class="flex flex-col gap-3">
-      <div class="bg-orange-500 flex justify-between py-1 text-white text-xl">
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >รถออก</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >เส้นทาง</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >ที่นั่งว่าง</label
-        >
-        <label class="w-1/3 flex justify-center items-center mr-9" for=""
-          >ราคา</label
-        >
-      </div>
-      <div class="border flex justify-between py-5 text-xl">
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >06:30</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >กรุงเทพฯ(หมอชิต2) - หาดใหญ่</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for="">100</label>
-        <label class="w-1/3 flex justify-center items-center" for="">300</label>
-        <input type="radio" class="mr-5 aspect-square w-5" name="back" id="" />
-      </div>
-      <div class="border flex justify-between py-5 text-xl">
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >06:30</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >กรุงเทพฯ(หมอชิต2) - หาดใหญ่</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for="">100</label>
-        <label class="w-1/3 flex justify-center items-center" for="">300</label>
-        <input type="radio" class="mr-5 aspect-square w-5" name="back" id="" />
-      </div>
-      <div class="border flex justify-between py-5 text-xl">
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >06:30</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for=""
-          >กรุงเทพฯ(หมอชิต2) - หาดใหญ่</label
-        >
-        <label class="w-1/3 flex justify-center items-center" for="">100</label>
-        <label class="w-1/3 flex justify-center items-center" for="">300</label>
-        <input type="radio" class="mr-5 aspect-square w-5" name="back" id="" />
-      </div>
-    </div>
+    <Table hoverable={true}>
+      <TableHead class="bg-[#4F75FF] rounded-md py-2 text-lg text-center text-white">
+          <TableHeadCell>รถออก</TableHeadCell>
+          <TableHeadCell>เส้นทาง</TableHeadCell>
+          <TableHeadCell>ที่นั่งว่าง</TableHeadCell>
+          <TableHeadCell>ราคา</TableHeadCell>
+          <TableHeadCell>
+              <span class="sr-only">Action</span>
+          </TableHeadCell>
+      </TableHead>
+      <TableBody>
+          <TableBodyRow class="text-center">
+            <TableBodyCell>06:30</TableBodyCell>
+            <TableBodyCell>กรุงเทพฯ(หมอชิต2) - หาดใหญ่</TableBodyCell>
+            <TableBodyCell>100</TableBodyCell>
+            <TableBodyCell>300.00 บาท</TableBodyCell>
+            <TableBodyCell>
+              <Radio name="back"></Radio>
+            </TableBodyCell>
+          </TableBodyRow>
+          <TableBodyRow class="text-center">
+            <TableBodyCell>06:30</TableBodyCell>
+            <TableBodyCell>กรุงเทพฯ(หมอชิต2) - หาดใหญ่</TableBodyCell>
+            <TableBodyCell>100</TableBodyCell>
+            <TableBodyCell>300.00 บาท</TableBodyCell>
+            <TableBodyCell>
+              <Radio name="back"></Radio>
+            </TableBodyCell>
+          </TableBodyRow>
+      </TableBody>
+  </Table>
   </div>
   <div class="flex justify-center my-5">
     <a
