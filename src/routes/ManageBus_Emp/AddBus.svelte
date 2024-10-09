afc0fc481671685caf0f2908bbf9a8d454b78a36
Pimmada Chirawat <83860415+pimsleepyy@users.noreply.github.com>
2024-10-09 21:25:44 +0700

    pim

---
+++ src/routes/ManageBus_Emp/AddBus.svelte
@@ -0,0 +1,354 @@
+<script>
+  let inputs = [{}]; // Array to store input values
+
+  function addInput() {
+    // Add a new empty input field
+    inputs = [...inputs, {}];
+  }
+
+  function handleInputChange(event, index) {
+    // Update the specific input field's value
+    inputs[index] = event.target.value;
+  }
+
+  function submitForm() {
+    // Do something with form data (e.g., submit)
+    console.log(inputs);
+  }
+</script>
+
+<div>
+  <form style="margin: 4% 5%;">
+    <div class="space-y-12">
+      <div class="border-b border-gray-900/10 pb-12">
+        <h2 class="text-xl font-semibold leading-7 text-gray-900">
+          เพิ่มเที่ยวรถ
+        </h2>
+        <p class="mt-1 text-sm leading-6 text-gray-600">
+          กรุณากรอกรายละเอียดเที่ยวรถที่ต้องการเพิ่ม
+        </p>
+
+        <div class="sm:col-span-6 mt-8">
+          <label
+            for="first-name"
+            class="block text-base font-medium leading-6 text-gray-900"
+            >ชื่อเที่ยวรถ</label
+          >
+          <div class="mt-2">
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              autocomplete="given-name"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6">
+          <div class="sm:col-span-3">
+            <label
+              for="first-name"
+              class="block text-base font-medium leading-6 text-gray-900"
+              >สถานีต้นทาง</label
+            >
+            <div class="mt-2">
+              <select
+                  id="countries"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                <option selected>เลือกสถานี</option>
+                <option value="bkk">กรุงเทพ</option>
+                </select>
+            </div>
+          </div>
+
+          <div class="sm:col-span-3">
+            <label
+              for="last-name"
+              class="block text-base font-medium leading-6 text-gray-900"
+              >สถานีปลายทาง</label
+            >
+            <div class="mt-2">
+              <select
+                  id="countries"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  <option selected>เลือกสถานี</option>
+                  <option value="bkk">กรุงเทพ</option>
+                </select>
+            </div>
+          </div>
+
+          <div class="sm:col-span-3">
+            <label
+              for="start-time"
+              class="block mb-2 text-base font-medium text-gray-900 dark:text-white"
+              >เวลาออกเดินทาง</label
+            >
+            <div class="relative">
+              <div
+                class="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none"
+              >
+                <svg
+                  class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </div>
+              <input
+                type="time"
+                id="start-time"
+                class="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                min="09:00"
+                max="18:00"
+                value="00:00"
+                required
+              />
+            </div>
+          </div>
+
+          <div class="sm:col-span-3">
+            <label
+              for="start-time"
+              class="block mb-2 text-base font-medium text-gray-900 dark:text-white"
+              >เวลาถึงปลายทาง</label
+            >
+            <div class="relative">
+              <div
+                class="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none"
+              >
+                <svg
+                  class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </div>
+              <input
+                type="time"
+                id="start-time"
+                class="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                min="09:00"
+                max="18:00"
+                value="00:00"
+                required
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-2 sm:grid-cols-5">
+          <h2 class="text-base font-semibold leading-7 text-black mb-1">
+            พนักงานผู้รับผิดชอบ
+          </h2>
+
+          {#each inputs as input, index}
+            <div class="sm:col-span-1 sm:col-start-1">
+              <label
+                for="city"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >รหัสพนักงาน</label
+              >
+              <div class="mt-2">
+                <input
+                  type="text"
+                  name="city"
+                  id="city"
+                  autocomplete="address-level2"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-1">
+              <label
+                for="region"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >ชื่อจริง</label
+              >
+              <div class="mt-2">
+                <input
+                  disabled
+                  type="text"
+                  name="region"
+                  id="region"
+                  autocomplete="address-level1"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-1">
+              <label
+                for="postal-code"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >นามสกุล</label
+              >
+              <div class="mt-2">
+                <input
+                  disabled
+                  type="text"
+                  name="postal-code"
+                  id="postal-code"
+                  autocomplete="postal-code"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-1">
+              <label
+                for="postal-code"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >เบอร์โทรศัพท์</label
+              >
+              <div class="mt-2">
+                <input
+                  disabled
+                  type="text"
+                  name="postal-code"
+                  id="postal-code"
+                  autocomplete="postal-code"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-1">
+              <label
+                for="postal-code"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >ตำแหน่ง</label
+              >
+              <div class="mt-2">
+                <select
+                  disabled
+                  id="countries"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  <option selected>เลือกตำแหน่ง</option>
+                  <option value="rider">หนักงานขับรถ</option>
+                  <option value="checker">พนักงานตรวจตั๋ว</option>
+                  <option value="caretaker">พนักงานดูแล/ทำความสะอาด</option>
+                </select>
+              </div>
+            </div>
+          {/each}
+
+          <div class="sm:col-span-1">
+            <button
+              on:click={addInput}
+              type="button"
+              class=" bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-2 mt-2 mb-6 rounded inline-flex items-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4 mr-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+              <span class="text-sm">เพิ่มพนักงาน</span>
+            </button>
+          </div>
+
+          <div class="col-span-full mb-6">
+            <label
+              for="about"
+              class="block text-base font-medium leading-6 text-gray-900"
+              >รายละเอียด</label
+            >
+            <div class="mt-2">
+              <textarea
+                id="about"
+                name="about"
+                rows="3"
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              ></textarea>
+            </div>
+          </div>
+
+          <div class="col-span-full">
+            <label
+              for="cover-photo"
+              class="block text-base font-medium leading-6 text-gray-900"
+              >รูปภาพ</label
+            >
+            <div
+              class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
+            >
+              <div class="text-center">
+                <svg
+                  class="mx-auto h-12 w-12 text-gray-300"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  data-slot="icon"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                  <label
+                    for="file-upload"
+                    class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>Upload a file</span>
+                    <input
+                      id="file-upload"
+                      name="file-upload"
+                      type="file"
+                      class="sr-only"
+                    />
+                  </label>
+                  <p class="pl-1">or drag and drop</p>
+                </div>
+                <p class="text-xs leading-5 text-gray-600">
+                  PNG, JPG, GIF up to 10MB
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="button"
+          class="text-sm font-semibold leading-6 text-gray-900">Cancel</button
+        >
+        <button
+          type="submit"
+          class="rounded-md bg-orange-500  px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-600  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >Save</button
+        >
+      </div>
+    </div>
+  </form>
+</div>
---
+++ src/routes/ManageBus_Emp/AddStation.svelte
@@ -0,0 +1,140 @@
+<script>
+</script>
+
+<div>
+    <form style="margin: 4% 5%;">
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+                <h2 class="text-xl font-semibold leading-7 text-gray-900">
+                    เพิ่มจุดขึ้นรถ-ลงรถ
+                </h2>
+                <p class="mt-1 text-sm leading-6 text-gray-600">
+                    กรุณากรอกรายละเอียดจุดขึ้นรถ-ลงรถที่ต้องการเพิ่ม
+                </p>
+
+                <div
+                    class="mt-6 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6"
+                >
+                    <div class="sm:col-span-3">
+                        <label
+                            for="first-name"
+                            class="block text-base font-medium leading-6 text-gray-900"
+                            >ชื่อ</label
+                        >
+                        <div class="mt-2">
+                            <input
+                                type="text"
+                                name="first-name"
+                                id="first-name"
+                                autocomplete="given-name"
+                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-3">
+                        <label
+                            for="last-name"
+                            class="block text-base font-medium leading-6 text-gray-900"
+                            >รายละเอียด</label
+                        >
+                        <div class="mt-2">
+                            <input
+                                type="text"
+                                name="last-name"
+                                id="last-name"
+                                autocomplete="family-name"
+                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-3">
+                        <label
+                            for="start-time"
+                            class="block mb-2 text-base font-medium text-gray-900 dark:text-white"
+                            >อำเภอ</label
+                        >
+                        <div class="relative">
+                            <div
+                                class="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none"
+                            >
+                                <svg
+                                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                                        clip-rule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <input
+                                type="time"
+                                id="start-time"
+                                class="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                min="09:00"
+                                max="18:00"
+                                value="00:00"
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-3">
+                        <label
+                            for="start-time"
+                            class="block mb-2 text-base font-medium text-gray-900 dark:text-white"
+                            >จังหวัด</label
+                        >
+                        <div class="relative">
+                            <div
+                                class="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none"
+                            >
+                                <svg
+                                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                                        clip-rule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <input
+                                type="time"
+                                id="start-time"
+                                class="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                min="09:00"
+                                max="18:00"
+                                value="00:00"
+                                required
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-6 flex items-center justify-end gap-x-6">
+                <button
+                    type="button"
+                    class="text-sm font-semibold leading-6 text-gray-900"
+                    >Cancel</button
+                >
+                <button
+                    type="submit"
+                    class="rounded-md bg-orange-500  px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >Save</button
+                >
+            </div>
+        </div>
+    </form>
+</div>
