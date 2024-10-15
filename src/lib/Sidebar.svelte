a7bde601deb7c75e84148e3d1f8230a976e3a790
Bla_ckB <50193121+BlackBird-Coding@users.noreply.github.com>
2024-10-15 22:02:02 +0700

    fix

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,5 +1,6 @@
 <script>
   import { Steps } from "svelte-steps";
+  import { onMount } from "svelte";
   let steps = [
     { text: "ค้นหาเที่ยวรถ" },
     { text: "เลือกเที่ยวรถ" },
@@ -10,8 +11,28 @@
   let line = "0.35rem";
   let clickable = false;
   export let current = 0;
+  let isMenuOpen = true;
+
+  onMount(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1024) {
+        isMenuOpen = true;
+      } else {
+        isMenuOpen = false;
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
 </script>
 
-<div class="h-screen flex flex-shrink-0">
-  <Steps primary="#30BD7A" vertical {steps} {line} {clickable} {current} />
-</div>
+{#if isMenuOpen}
+  <div class="h-screen flex flex-shrink-0">
+    <Steps primary="#30BD7A" vertical {steps} {line} {clickable} {current} />
+  </div>
+{/if}
